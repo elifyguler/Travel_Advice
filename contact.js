86726d2a663c5bfdb5c7da36cb313c80b9385d97
elifyguler <elifguler93@yandex.com>
2022-09-21 14:05:09 +0100

    fourth commit

--- contact.js
+++ contact.js
@@ -1,9 +1,40 @@
 function contactform()
 {
-  let alert =document.getElementById("alert");
-  let success =document.getElementById("success");
+  var customeremail = document.getElementById("eaddress").value;
+  var customerfullname = document.getElementById("fname").value + " " +document.getElementById("lname").value;
+  var country = document.getElementById("country").value;
+  var message = document.getElementById("subject").value;
 
-  alert.style.display = "block";
+  let information = "name="+customerfullname+"&kmail="+customeremail+"&country="+country+"&message="+message + "&subject=Your message is arrived to us.";
+
+
+  let url = "http://besttraveladvice.co.uk/mailsend/email.php?";
+
+//check value is empty or not
+  if (customeremail=="" || message=="")
+  {
+    alert("You have to enter your email and message. Please enter them.")
+  }
+else{
+  var req = new XMLHttpRequest();
+  req.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {    
+         if(req.responseText=="ok")
+          { 
+              document.getElementById("success").style.display="block";
+          }
+      
+         else
+         {
+              document.getElementById("alert").style.display="block";
+          } 
+      }
+    } 
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  };
+  req.open("GET", url+information, true);
+  req.send();
 
 }
 
